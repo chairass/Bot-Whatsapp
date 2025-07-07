71d8d5c03728625f6c43b3b6b8d0831d12f4a686
chairass <chairassacramento@gmail.com>
2025-07-07 01:59:23 -0300

    Alterando o nome do bot

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 exports.PREFIX ="/";
 exports.BOT_EMOJI = "👺";
-exports.BOT_NAME = "Xaxado Boy"
+exports.BOT_NAME = "Xaxado Bot"
 exports.TIMEOUT_IN_MILLISECONDS_BY_EVENT = 500;
 
 exports.TEMP_DIR = path.resolve(__dirname, "..", "assets", "temp");
